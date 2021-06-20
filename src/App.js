6b9876227c4505593d27791cdb7cec585c396dff
kongkx <kongkx.yang@gmail.com>
2021-06-20 11:05:22 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactGA from 'react-ga'
+import { useEffect, useState } from 'react'
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
+
+import logo from './logo.svg'
+import './App.css'
+
+// const TRACKING_ID = '252869480' // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)
 
 function App() {
+  const [clientId, setClientId] = useState()
+  const [fingerprint, setFingerprint] = useState()
+  useEffect(() => {
+    ReactGA.ga(function (tracker) {
+      setClientId(tracker.get('clientId'))
+    })
+    FingerprintJS.load()
+      .then((fp) => {
+        return fp.get().then((result) => {
+          setFingerprint(result.visitorId)
+        })
+      })
+      .catch((err) => {
+        window.alert(err.message)
+      })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          GA clientId: <code>{clientId}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Fingerprint: <code>{fingerprint}</code>
+        </p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
